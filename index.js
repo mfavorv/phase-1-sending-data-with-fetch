0de3584fb08c2097427e7c3f3ded9efab019339c
Favor Vincent <vfavorm77@gmail.com>
2024-04-07 07:35:25 +0300

    JS sending data with fetch lab complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+function submitData(name, email){
+ return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body:JSON.stringify({
+          name : "Steve",
+          email: "steve@steve.com"
+        })   
+    })
+    .then((response) => response.json())
+    .then(data => {
+        const newId = data.id;
+        const idElement = document.createElement("p");
+        idElement.textContent = `New ID: ${newId}`;
+        document.body.appendChild(idElement)})
+    .catch(error => {
+        const errorElement = document.createElement("p");
+        errorElement.textContent = `Error: ${error.message}`;
+        document.body.appendChild(errorElement);
+    })
+}
+
+submitData(name, email)
